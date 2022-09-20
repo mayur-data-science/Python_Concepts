cd8b85eaf9c85d6c2e2d5f10f50952ee5a5519de
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-09-21 01:54:30 +0530

    Function Aliasing

--- function_aliasing.py
+++ function_aliasing.py
@@ -1,3 +1,5 @@
+# FUNCTION ALIASING:
+#        For existing function we can give another name this is nothing but function aliasing
 
 # In python everything is treated as an object
 # Even functions also internally treated as object only
@@ -8,13 +10,13 @@ def add():
     print("hello world")
 
 print(add) # <function add at 0x000001BD7778AF80>
-# "add" is an referance variable which is pointing to "function" type object "at" so and so location.
+# "add" is an referance variable which is pointing to "function" type object "at" so and so memory location.
 print(id(add)) # 1913264844672
-# on this(0x000001BD7778AF80) location id(1913264844672) is located.
+# In this(0x000001BD7778AF80) memory location, id(1913264844672) of an function type object is present.
 
 # Can we create another referance variable for add() function?
 # YES, why not, we can create another referance variable..
-# ..which is pointing to same function type object which already add reference variable is pointing.
+# ..which is pointing to same function type object which already add reference variable is pointing to.
 
 add1 = add # what ever object pointed by add, for same object can you please assign add1 refrance variable also
 print(add1) # <function add at 0x000001BD7778AF80>
@@ -23,13 +25,11 @@ print(id(add)) # 1913264844672
 # This is called function aliasing.
 add() # calling function add at 0x000001BD7778AF80 location.
 # or you can also call like.
-add1() # calling function add at 0x000001BD7778AF80 location.
-# FUNCTION ALIASING:
-#        For existing function we can give another name this is nothing but function aliasing
+add1() # calling function add which is present at 0x000001BD7778AF80 memory location.
 
 del add # deleted add referance variable for function type object.
 # this function type object is not available for garbage collection automatically because there is another ...
 #...referance variable is assigned to it i.e add1
 # we can access add() functin by add1 referance variable.
-add() # NameError: name 'add' is not defined. Did you mean: 'add1'?
+#add() # NameError: name 'add' is not defined.
 add1() # happly called add() function.
