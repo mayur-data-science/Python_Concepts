53f42914edcf4367a73a2de2919c6789073ac246
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-09-20 22:48:02 +0530

    Explaining Aliasing & Function Aliasing

--- function_aliasing.py
+++ function_aliasing.py
@@ -1,4 +1,18 @@
-"""# In python everything is treated as an object
+# In python programming, the second name given to a piece of data is known as an alias. 
+# Aliasing happens when the value of one variable is assigned to another variable because variables are just names that store references to actual value.
+# Example:
+
+first_variable = "PYTHON"
+print("Value of first:", first_variable) # Value of first: PYTHON
+print("Reference of first:", id(first_variable)) # Reference of first: 2610710821296 
+
+print("--------------")
+
+second_variable = first_variable # making an alias
+print("Value of second:", second_variable) # Value of second: PYTHON
+print("Reference of second:", id(second_variable)) # Reference of second: 2610710821296 
+
+# In python everything is treated as an object
 # Even functions also internally treated as object only
 # proof: 
 
@@ -23,4 +37,4 @@ add() # calling function add at 0x000001BD7778AF80 location.
 # or you can also call like.
 add1() # calling function add at 0x000001BD7778AF80 location.
 # FUNCTION ALIASING:
-#        For existing function we can give another name this is nothing but function aliasing"""
+#        For existing function we can give another name this is nothing but function aliasing
