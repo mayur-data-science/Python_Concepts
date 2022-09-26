97ea557380ce4154669169bc67178e6ced5d2772
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-09-26 23:17:12 +0530

    FUNCTION ALIASING

--- function_aliasing.py
+++ function_aliasing.py
@@ -11,8 +11,8 @@ def add():
 
 print(add) # <function add at 0x000001BD7778AF80>
 # "add" is an referance variable which is pointing to "function" type object "at" so and so memory location.
-print(id(add)) # 1913264844672
-# In this(0x000001BD7778AF80) memory location, object(id is 1913264844672) of an function type is present.
+print(id(add)) # 1913264844672 i.e Hexadecimal representation is 0x000001BD7778AF80
+# In this(1913264844672 i.e 0x000001BD7778AF80) memory location, object of an function type is present.
 
 # Can we create another referance variable for add() function?
 # YES, why not, we can create another referance variable..
@@ -20,15 +20,15 @@ print(id(add)) # 1913264844672
 
 add1 = add # what ever object pointed by add, for same object can you please assign add1 refrance variable also
 print(add1) # <function add at 0x000001BD7778AF80>
-print(id(add))  # 1913264844672
-print(id(add1)) # 1913264844672
+print(id(add))  # 1913264844672 i.e Hexadecimal representation is 0x000001BD7778AF80
+print(id(add1)) # 1913264844672 i.e Hexadecimal representation is 0x000001BD7778AF80
 # now add1 referance variable also pointing to the same object which add is pointing to.
 # This is called function aliasing.
 add() # calling function add at 0x000001BD7778AF80 location.
 # or you can also call like.
 add1() # calling function add which is present at 0x000001BD7778AF80 memory location.
 
-del add # deleted add referance variable for function type object.
+del add # deleted the add referance variable for function type object.
 # this function type object is not available for garbage collection automatically because there is another ...
 #...referance variable is assigned to it i.e "add1"
 # we can access "add()" function by "add1" referance variable.
